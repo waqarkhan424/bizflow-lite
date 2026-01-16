6a0ffedd69929decc8164e7980a8c1e795e38d3e
Waqar Khan <waqar.mrwt424@gmail.com>
2026-01-16 20:14:00 +0500

    setup

--- lib/get-current-user.ts
+++ lib/get-current-user.ts
@@ -6,6 +6,8 @@ export async function getCurrentUser() {
   const cookieStore = await cookies();
   const token = cookieStore.get(SESSION_COOKIE_NAME)?.value;
 
+ console.log("getCurrentUser token:", token);
+
   if (!token) return null;
 
   const session = await prisma.session.findUnique({
