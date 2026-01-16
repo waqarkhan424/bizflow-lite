eb76b4ddeb4908b36dd8dafeb4da93cea1a08ddf
Waqar Khan <waqar.mrwt424@gmail.com>
2026-01-16 20:20:45 +0500

    setup

--- lib/get-current-user.ts
+++ lib/get-current-user.ts
@@ -6,7 +6,6 @@ export async function getCurrentUser() {
   const cookieStore = await cookies();
   const token = cookieStore.get(SESSION_COOKIE_NAME)?.value;
 
- console.log("getCurrentUser token:", token);
 
   if (!token) return null;
 
