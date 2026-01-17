6bec6262ae297947f9332c27a07c483fc3c368ca
Waqar Khan <waqar.mrwt424@gmail.com>
2026-01-17 21:23:56 +0500

    setup

--- proxy.ts
+++ proxy.ts
@@ -2,10 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { SESSION_COOKIE_NAME } from "@/lib/auth";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const token = req.cookies.get(SESSION_COOKIE_NAME)?.value;
 
-  // If no cookie -> send to login
   if (!token) {
     const url = req.nextUrl.clone();
     url.pathname = "/login";
@@ -15,7 +14,6 @@ export function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
-// Apply middleware only on these routes:
 export const config = {
   matcher: ["/dashboard/:path*"],
 };
